2537f98b16bdac6d58e7b1fe1cb03ee7daec0a63
wangchow <51769493+wangchow@users.noreply.github.com>
2021-08-10 19:10:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,3 +21,4 @@
 #git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
